f218eb02821c3b882ed711b80e240b324889cd9f
Roma Bulani <romabulani@gmail.com>
2022-04-16 13:40:24 +0530

    feat : routes and context provider

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { makeServer } from "./server";
+import { theme } from "styles";
+import "assets";
+// Call make Server
+makeServer();
+console.log(theme.styles);
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
