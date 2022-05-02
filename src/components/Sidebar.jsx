8b3aef43935ce2f64ec4635aec02855db38208e9
Roma Bulani <romabulani@gmail.com>
2022-05-02 10:25:30 +0530

    chore: components folder for common components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,80 @@
+import { Link, NavLink } from "react-router-dom";
+import { Flex, Text, useColorModeValue, Box, Avatar } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { sidebarStyle } from "styles";
+import { PostModal } from "features";
+
+function Sidebar() {
+  const getActiveStyle = ({ isActive }) =>
+    isActive
+      ? {
+          backgroundColor: useColorModeValue("#d1d8ff", "#2d2d3a"),
+          display: "block",
+        }
+      : { backgroundColor: "transparent", display: "block" };
+
+  const getNavItem = (iconName, text) => (
+    <Flex columnGap="1rem" alignItems="center" padding="0.8rem 2rem">
+      <FontAwesomeIcon icon={iconName} />
+      <Text>{text}</Text>
+    </Flex>
+  );
+
+  return (
+    <>
+      <Flex
+        {...sidebarStyle}
+        borderColor={useColorModeValue("gray.300", "gray.700")}
+      >
+        <Box pos="sticky" top="3.5rem">
+          <NavLink to="/" style={getActiveStyle}>
+            {getNavItem("house", "Home")}
+          </NavLink>
+          <NavLink to="/notifications" style={getActiveStyle}>
+            {getNavItem("bell", "Notifications")}
+          </NavLink>
+          <NavLink to="/bookmarks" style={getActiveStyle}>
+            {getNavItem("bookmark", "Bookmarks")}
+          </NavLink>
+          <NavLink to="/profile" style={getActiveStyle}>
+            {getNavItem("user", "Profile")}
+          </NavLink>
+          <PostModal />
+        </Box>
+      </Flex>
+
+      <Flex
+        position="fixed"
+        bottom="0"
+        zIndex="7"
+        width="100vw"
+        justifyContent="space-around"
+        height="2rem"
+        padding="5px"
+        backgroundColor={useColorModeValue("white.900", "gray.900")}
+        display={{ base: "flex", md: "none" }}
+      >
+        <Link to="/">
+          <FontAwesomeIcon icon="home" />
+        </Link>
+        <Link to="/notifications">
+          <FontAwesomeIcon icon="bell" />
+        </Link>
+        <PostModal />
+        <Link to="/bookmarks">
+          <FontAwesomeIcon icon="bookmark" />
+        </Link>
+        <Link to="/profile">
+          <Avatar
+            src="https://randomuser.me/api/portraits/women/27.jpg"
+            alt="profile-image"
+            size="xs"
+            name="Adarsh Balika"
+          />
+        </Link>
+      </Flex>
+    </>
+  );
+}
+
+export { Sidebar };
