5121a949896d106a738097da217c09653cedb917
Roma Bulani <romabulani@gmail.com>
2022-05-03 21:08:36 +0530

    feat: Added Edit profile

--- src/features/profile/components/ProfileCard.jsx
+++ src/features/profile/components/ProfileCard.jsx
@@ -13,63 +13,82 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { postCardStyle } from "styles";
 import { UpdateProfileForm, logoutUser } from "features";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { loadUserDetails } from "features";
 
 function ProfileCard() {
+  const { username } = useParams();
   const dispatch = useDispatch();
+  const { profileDetails } = useSelector((state) => state.profile);
   const { authUser } = useSelector((state) => state.authentication);
+  useEffect(() => {
+    dispatch(loadUserDetails(username));
+  }, [username]);
 
   return (
-    <Flex {...postCardStyle} boxShadow="none">
-      <Flex w="100%" marginTop="1" padding="2">
-        <Avatar
-          src="https://randomuser.me/api/portraits/women/27.jpg"
-          size="lg"
-          name="Adarsh Balika"
-        />
-        <Flex w="100%" m="2" flexDirection="column" rowGap="1">
-          <Flex justifyContent="space-between" flexWrap="wrap">
-            <Flex flexDirection="column">
-              <Text fontWeight="bold">{`${authUser.firstName} ${authUser.lastName}`}</Text>
-              <Text>{`@${authUser.username}`}</Text>
+    <>
+      {profileDetails && (
+        <Flex {...postCardStyle} boxShadow="none">
+          <Flex w="100%" marginTop="1" padding="2">
+            <Avatar
+              src={profileDetails.avatarUrl}
+              size="lg"
+              name={`${profileDetails.firstName} ${profileDetails.lastName}`}
+            />
+            <Flex w="100%" m="2" flexDirection="column" rowGap="1">
+              <Flex justifyContent="space-between" flexWrap="wrap">
+                <Flex flexDirection="column">
+                  <Text fontWeight="bold">{`${profileDetails.firstName} ${profileDetails.lastName}`}</Text>
+                  <Text>{`@${profileDetails.username}`}</Text>
+                </Flex>
+                {authUser.username === profileDetails.username && (
+                  <Flex alignItems="center">
+                    <UpdateProfileForm />
+                    <Tooltip label="Logout" aria-label="A tooltip">
+                      <IconButton
+                        variant="iconButton"
+                        icon={<FontAwesomeIcon icon="sign-out" />}
+                        onClick={() => dispatch(logoutUser())}
+                      />
+                    </Tooltip>
+                  </Flex>
+                )}
+                {authUser.username !== profileDetails.username && (
+                  <Button variant="outline" h="2rem">
+                    Follow
+                  </Button>
+                )}
+              </Flex>
+              <Text>{profileDetails.bio}</Text>
+              <Flex justifyContent="space-between" flexWrap="wrap">
+                <Link href={profileDetails.website} isExternal target="_blank">
+                  {profileDetails.website}
+                </Link>
+                <Box>
+                  <FontAwesomeIcon icon="calendar" color="#718096" />
+                  <span style={{ color: "#718096" }}>{` ${new Date(
+                    profileDetails.createdAt
+                  )
+                    .toDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                    .slice(4)}`}</span>
+                </Box>
+              </Flex>
+              <Flex justifyContent="space-between" flexWrap="wrap">
+                <Button variant="link">5 posts</Button>
+                <Button variant="link">40 followers</Button>
+                <Button variant="link">40 following</Button>
+              </Flex>
             </Flex>
-            <Flex alignItems="center">
-              <UpdateProfileForm />
-              <Tooltip label="Logout" aria-label="A tooltip">
-                <IconButton
-                  variant="iconButton"
-                  icon={<FontAwesomeIcon icon="sign-out" />}
-                  onClick={() => dispatch(logoutUser())}
-                />
-              </Tooltip>
-            </Flex>
           </Flex>
-          <Text>Be yourself!</Text>
-          <Flex justifyContent="space-between" flexWrap="wrap">
-            <Link href="https://google.com" isExternal>
-              https://google.com
-            </Link>
-            <Box>
-              <FontAwesomeIcon icon="calendar" color="#718096" />
-              <span style={{ color: "#718096" }}>{` ${new Date(
-                authUser.createdAt
-              )
-                .toDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })
-                .slice(4)}`}</span>
-            </Box>
-          </Flex>
-          <Flex justifyContent="space-between" flexWrap="wrap">
-            <Button variant="link">5 posts</Button>
-            <Button variant="link">40 followers</Button>
-            <Button variant="link">40 following</Button>
-          </Flex>
+          <Divider />
         </Flex>
-      </Flex>
-      <Divider />
-    </Flex>
+      )}
+    </>
   );
 }
 
--- src/features/profile/components/UpdateProfileForm.jsx
+++ src/features/profile/components/UpdateProfileForm.jsx
@@ -13,16 +13,75 @@ import {
   FormControl,
   FormLabel,
   Input,
+  AvatarBadge,
+  Text,
 } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { editUserProfile } from "features";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import { inputLeftWrapperStyle, inputWrapperStyle } from "styles";
+import { loadUserDetails } from "features";
+
 function UpdateProfileForm() {
   const { isOpen, onClose, onOpen } = useDisclosure();
+  const { authUser, authToken } = useSelector((state) => state.authentication);
+  const [website, setWebsite] = useState(authUser.website);
+  const [bio, setBio] = useState(authUser.bio);
+  const [avatarUrl, setAvatarUrl] = useState(authUser.avatarUrl);
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(loadUserDetails(authUser.username)), [authUser]);
+  const resetFields = () => {
+    onClose();
+    setWebsite(authUser.website);
+    setBio(authUser.bio);
+    setAvatarUrl(authUser.avatarUrl);
+  };
+
+  const uploadImage = async (image) => {
+    if (Math.round(image.size / 1024000) > 2)
+      toast.error("File size should be less than 2MB");
+    else {
+      const data = new FormData();
+      data.append("file", image);
+      data.append("upload_preset", process.env.REACT_APP_CLOUDINARY_API_KEY);
+      const requestOptions = {
+        method: "POST",
+        body: data,
+      };
+      await fetch(
+        "https://api.cloudinary.com/v1_1/dtrjdcrme/image/upload",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          setAvatarUrl(json.url);
+        })
+        .catch((error) => {
+          console.error(error);
+          toast.error("Image Uploading failed");
+        });
+    }
+  };
+
+  const updateDetails = (e) => {
+    e.preventDefault();
+    dispatch(
+      editUserProfile({ userDetails: { avatarUrl, website, bio }, authToken })
+    );
+    toast.success("Details Updated!");
+    onClose();
+  };
+
   return (
     <>
       <Button variant="outline" h="2rem" onClick={onOpen}>
         Edit Profile
       </Button>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="sm" m="2">
+      <Modal isOpen={isOpen} onClose={resetFields} isCentered size="sm" m="2">
         <ModalOverlay />
         <ModalContent w="90%">
           <ModalHeader>Edit Profile</ModalHeader>
@@ -33,25 +92,54 @@ function UpdateProfileForm() {
               boxShadow: "transparent",
             }}
           />
-          <ModalBody justifyContent="space-between">
-            <form>
+          <form onSubmit={updateDetails}>
+            <ModalBody justifyContent="space-between">
               <FormControl {...inputWrapperStyle}>
-                <FormLabel {...inputLeftWrapperStyle}>Avatar</FormLabel>
+                <Text {...inputLeftWrapperStyle}>Avatar</Text>
+
                 <Avatar
-                  src="https://randomuser.me/api/portraits/women/27.jpg"
+                  src={avatarUrl}
                   alt="profile-image"
                   size="md"
                   marginRight="2"
-                  name="Adarsh Balika"
-                />
+                  name={`${authUser.firstName} ${authUser.lastName}`}
+                >
+                  <AvatarBadge boxSize="1.5em" border="0">
+                    <FormControl>
+                      <FormLabel
+                        color={useColorModeValue("gray.600", "whiteAlpha.800")}
+                        cursor="pointer"
+                        position="absolute"
+                        right="-10px"
+                        bottom="0"
+                      >
+                        <FontAwesomeIcon icon="camera" />
+                      </FormLabel>
+                      <Input
+                        type="file"
+                        visibility="hidden"
+                        accept="image/*"
+                        onChange={(e) => uploadImage(e.target.files[0])}
+                      />
+                    </FormControl>
+                  </AvatarBadge>
+                </Avatar>
               </FormControl>
               <FormControl {...inputWrapperStyle}>
                 <FormLabel {...inputLeftWrapperStyle}>Name</FormLabel>
-                <Input variant="unstyled" value="Adarsh Balika" isReadOnly />
+                <Input
+                  variant="unstyled"
+                  value={`${authUser.firstName} ${authUser.lastName}`}
+                  isReadOnly
+                />
               </FormControl>
               <FormControl {...inputWrapperStyle}>
                 <FormLabel {...inputLeftWrapperStyle}>Username</FormLabel>
-                <Input variant="unstyled" value="adarshbalika" isReadOnly />
+                <Input
+                  variant="unstyled"
+                  value={authUser.username}
+                  isReadOnly
+                />
               </FormControl>
               <FormControl {...inputWrapperStyle}>
                 <FormLabel {...inputLeftWrapperStyle}>Bio</FormLabel>
@@ -59,17 +147,32 @@ function UpdateProfileForm() {
                   _focus={{
                     borderColor: useColorModeValue("purple.900", "purple.300"),
                   }}
-                  value="Be yourself!"
+                  value={bio}
+                  onChange={(e) => setBio(e.target.value)}
                 />
               </FormControl>
-            </form>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button mr={3} onClick={onClose} variant="solidPrimary">
-              Update
-            </Button>
-          </ModalFooter>
+              <FormControl {...inputWrapperStyle}>
+                <FormLabel {...inputLeftWrapperStyle}>Website</FormLabel>
+                <Input
+                  _focus={{
+                    borderColor: useColorModeValue("purple.900", "purple.300"),
+                  }}
+                  value={website}
+                  onChange={(e) => setWebsite(e.target.value)}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                mr={3}
+                variant="solidPrimary"
+                type="submit"
+                onClick={updateDetails}
+              >
+                Update
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
