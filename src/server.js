2ba24c74d9d0325bd67bf7050d86f1847ee06105
Roma Bulani <romabulani@gmail.com>
2022-05-03 21:08:11 +0530

    feat: cloudinary api passthrough

--- src/server.js
+++ src/server.js
@@ -72,7 +72,7 @@ export function makeServer({ environment = "development" } = {}) {
 
       // user routes (public)
       this.get("/users", getAllUsersHandler.bind(this));
-      this.get("/users/:userId", getUserHandler.bind(this));
+      this.get("/users/:username", getUserHandler.bind(this));
 
       // user routes (private)
       this.post("users/edit", editUserHandler.bind(this));
@@ -86,6 +86,11 @@ export function makeServer({ environment = "development" } = {}) {
       this.post(
         "/users/unfollow/:followUserId/",
         unfollowUserHandler.bind(this)
+      );
+      this.passthrough();
+      this.passthrough(
+        "https://api.cloudinary.com/v1_1/dtrjdcrme/image/upload",
+        ["post"]
       );
     },
   });
