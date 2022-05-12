aedba14947fe9120f96a018f56ae51d078a0f33f
Roma Bulani <romabulani@gmail.com>
2022-05-13 00:04:46 +0530

    feat: Dynamic Profile

--- src/features/profile/components/Profile.jsx
+++ src/features/profile/components/Profile.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect } from "react";
-import { Flex, Heading } from "@chakra-ui/react";
-import { loadUserPosts, Suggestions, ProfileCard, DisplayPost } from "features";
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { Suggestions, ProfileCard } from "features";
 import { Footer, Navigation, Sidebar } from "components";
 import {
   flexMiddleContainerStyle,
   flexMiddleOuterContainerStyle,
 } from "styles";
-import { useDispatch, useSelector } from "react-redux";
 
 function Profile() {
-  const { postsDetails } = useSelector((state) => state.profile);
-  const { authUser } = useSelector((state) => state.authentication);
-  const { posts } = useSelector((state) => state.posts);
-  const dispatch = useDispatch();
-  useEffect(() => dispatch(loadUserPosts(authUser.username)), [posts]);
-
   return (
     <>
       <Navigation />
@@ -22,13 +15,6 @@ function Profile() {
         <Sidebar />
         <Flex {...flexMiddleContainerStyle}>
           <ProfileCard />
-          {postsDetails.length > 0 ? (
-            postsDetails.map((post) => (
-              <DisplayPost key={post._id} post={post} />
-            ))
-          ) : (
-            <Heading>No Posts Yet!</Heading>
-          )}
         </Flex>
         <Suggestions />
       </Flex>
--- src/features/profile/components/ProfileCard.jsx
+++ src/features/profile/components/ProfileCard.jsx
@@ -8,6 +8,7 @@ import {
   Link,
   Divider,
   Tooltip,
+  Heading,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { postCardStyle } from "styles";
@@ -15,16 +16,49 @@ import { UpdateProfileForm, logoutUser } from "features";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { loadUserDetails } from "features";
+import {
+  loadUserDetails,
+  loadUserPosts,
+  followUser,
+  unfollowUser,
+  DisplayPost,
+} from "features";
+import { FollowersModal } from "./FollowersModal";
+import { FollowingModal } from "./FollowingModal";
 
 function ProfileCard() {
   const { username } = useParams();
   const dispatch = useDispatch();
   const { profileDetails } = useSelector((state) => state.profile);
-  const { authUser } = useSelector((state) => state.authentication);
+  const { followStatus } = useSelector((state) => state.users);
+  const { authUser, authToken } = useSelector((state) => state.authentication);
+  const { postsDetails } = useSelector((state) => state.profile);
+
   useEffect(() => {
-    dispatch(loadUserDetails(username));
-  }, [username]);
+    if (username) {
+      dispatch(loadUserDetails(username));
+      dispatch(loadUserPosts(username));
+    }
+  }, [username, authUser]);
+
+  const isFollowed = () =>
+    profileDetails.followers.some(
+      (currUser) => currUser.username === authUser.username
+    );
+
+  const followUnfollowClickHandler = () => {
+    isFollowed()
+      ? dispatch(
+          unfollowUser({
+            followUserId: profileDetails._id,
+            authToken,
+            dispatch,
+          })
+        )
+      : dispatch(
+          followUser({ followUserId: profileDetails._id, authToken, dispatch })
+        );
+  };
 
   return (
     <>
@@ -36,7 +70,7 @@ function ProfileCard() {
         >
           <Flex w="100%" marginTop="1" padding="2">
             <Avatar
-              src={profileDetails.avatarUrl}
+              src={profileDetails?.avatarUrl}
               size="lg"
               name={`${profileDetails.firstName} ${profileDetails.lastName}`}
             />
@@ -59,8 +93,13 @@ function ProfileCard() {
                   </Flex>
                 )}
                 {authUser.username !== profileDetails.username && (
-                  <Button variant="outline" h="2rem">
-                    Follow
+                  <Button
+                    variant="outline"
+                    h="2rem"
+                    onClick={followUnfollowClickHandler}
+                    disabled={followStatus === "pending"}
+                  >
+                    {isFollowed() ? "Following" : "Follow"}
                   </Button>
                 )}
               </Flex>
@@ -83,13 +122,22 @@ function ProfileCard() {
                 </Box>
               </Flex>
               <Flex justifyContent="space-between" flexWrap="wrap">
-                <Button variant="link">5 posts</Button>
-                <Button variant="link">40 followers</Button>
-                <Button variant="link">40 following</Button>
+                <Button variant="link">{postsDetails.length} posts</Button>
+                <FollowersModal followersList={profileDetails.followers} />
+                <FollowingModal followingList={profileDetails.following} />
               </Flex>
             </Flex>
           </Flex>
           <Divider />
+          {postsDetails.length > 0 ? (
+            postsDetails.map((post) => (
+              <DisplayPost key={post._id} post={post} />
+            ))
+          ) : (
+            <Heading textAlign="center" pt="4">
+              No Posts Yet!
+            </Heading>
+          )}
         </Flex>
       )}
     </>
